503b7760ef868183e463440e26466a4a374e29d5
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-09-29 16:27:25 +0200

    test

---
+++ ukol-1.py
@@ -0,0 +1,13 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+
+if baliky["B541X"] == True:
+    print("Balík byl předán kurýrovi"),
+else:
+    print("Balík zatím nebyl předán kurýrovi")
+
