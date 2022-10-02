afe88d3212353d8910a3a4722068a030fd689a25
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-10-02 10:45:23 +0200

    další verze

--- ukol-1.py
+++ ukol-1.py
@@ -6,19 +6,26 @@ baliky = {
     "B947X": False,
 }
 
-print(baliky.keys())
-
 if baliky["B541X"] == True:
     print("Balík byl předán kurýrovi"),
 else:
     print("Balík zatím nebyl předán kurýrovi")
 
-for key in baliky:
-    print(key)
-    if key == True:
-        print("Balik byl předán kurýrovi."),
-    else:
-        print("Balík zatím nebyl předán kurýrovi.")
+uz1 = baliky["B541X"]
+uz2 = baliky["B547X"]
+uz3 = baliky["B251X"]
+uz4 = baliky["B501X"]
+uz5 = baliky["B947X"]
+
+if uz1 == True:
+    print("Balik byl předán kurýrovi.")
+else:
+    print("Balík zatím nebyl předán kurýrovi.")
+
+if baliky.values == True:
+    print("Balik byl předán kurýrovi."),
+else:
+    print("Balík zatím nebyl předán kurýrovi.")
 
 
 
