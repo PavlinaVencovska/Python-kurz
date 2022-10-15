5f40e6ae8b383b90a8ec726fda75c7c9748b8581
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-10-15 10:53:43 +0200

    Se zadáním

--- ukol-1fin.py
+++ ukol-1fin.py
@@ -1,3 +1,10 @@
+# Níže máš slovník, který obsahuje kódy balíků s informací, zda již byl balík předán kurýrovi k doručení. 
+# Pokud byl předán, má hodnotu True, v opačném případě má hodnotu False.
+
+# Napiš program pro operátora společnosti, který poskytuje informaci, zda byl balík předán kurýrovi. 
+# Nejprve se uživatele zeptej na kód balíku. 
+# Následně podle hodnoty ve slovníku vypiš větu Balík byl předán kurýrovi nebo Balík zatím nebyl předán kurýrovi.
+
 baliky = {
     "B541X": True,
     "B547X": False,
@@ -13,3 +20,5 @@ if baliky[kod]:
     print("balík byl předán kurýrovi.")
 else: 
     print("balík zatím nebyl předán kurýrovi.")
+
+
