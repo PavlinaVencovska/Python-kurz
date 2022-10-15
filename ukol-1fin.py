d04afde62eac1f615d40c00a415a9b2622002124
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-10-15 11:14:35 +0200

    Se zadáním

--- ukol-1fin.py
+++ ukol-1fin.py
@@ -1,7 +1,7 @@
 # Zadání:
+
 # Níže máš slovník, který obsahuje kódy balíků s informací, zda již byl balík předán kurýrovi k doručení. 
 # Pokud byl předán, má hodnotu True, v opačném případě má hodnotu False.
-
 # Napiš program pro operátora společnosti, který poskytuje informaci, zda byl balík předán kurýrovi. 
 # Nejprve se uživatele zeptej na kód balíku. 
 # Následně podle hodnoty ve slovníku vypiš větu Balík byl předán kurýrovi nebo Balík zatím nebyl předán kurýrovi.
