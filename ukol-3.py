4ca4a0aa839416f0607e33bc52235513dbd629f8
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-10-18 10:09:45 +0200

    Ještě tam trochu bojuju s tím zaokrouhlováním, ale jinak snad ok.

---
+++ ukol-3.py
@@ -0,0 +1,45 @@
+# Zadání:
+# Uvažuj, že píšeš jednoduchou aplikaci pro zasílání SMS zpráv. 
+# Napiš program, který provede následující činnosti:
+
+# Zeptá se uživatele na číslo, kam chce zprávu zaslat a ověří, že číslo má správný formát.
+# Zeptá se uživatele na zprávu, kterou chce zaslat. Následně vypíše, kolik zpráva bude stát.
+
+# Tvůj program bude obsahovat dvě funkce:
+
+# První funkce ověří délku telefonního čísla. 
+# Uvažuj, že telefonní číslo může být devítimístné nebo třináctimístné (pokud je na začátku předvolba "+420"). Nemusíte kontrolovat, zda uživatel zadal skutečně číslo, či zda jsou tam i jiné znaky. To jsme v kurzu zatím neřešili. Pokud je číslo platné, funkce vrátí hodnotu True, jinak vrátí hodnotu False.
+# Druhá funkce spočte cenu zprávy. Uživatel platí 3 Kč za každých započatých 180 znaků.
+
+# Tvůj program nejprve ověří pomocí první funkce správnost telefonního čísla. 
+# Pokud není platné, vypíše chybu, v opačném případě se zeptá na text zprávy a pomocí druhé funkce určí její cenu, kterou vypíše uživateli.
+
+prijemce = str(input("Zadej telefonni cislo: "))
+
+def delka_tel_cisla(prijemce):
+    if len(prijemce) == 9 or len(prijemce) == 13:
+        return True
+    else: 
+        print("Cislo nema spravny format.")
+        return False
+
+if delka_tel_cisla(prijemce):
+    zprava = str(input("Napis zpravu: "))
+else:
+    prijemce = str(input("Zkus to znovu."))
+
+import math
+
+def cena_zpravy(zprava):
+    if len(zprava) % 180 == 0:
+        fin_cena = (len(zprava) / 180) * 3 
+        print(f"Cena zpravy je : {fin_cena}")
+    else:
+        fin_cena = ((math.floor(len(zprava) / 180)) * 3 + 3)
+        print(f"Cena zpravy je: {fin_cena}")
+
+cena_zpravy(zprava)
+
+
+
+
