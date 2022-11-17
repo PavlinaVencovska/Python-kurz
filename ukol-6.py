954baac770d74c644ccf97907afa5774c9436d2d
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-11-17 12:52:25 +0100

    File Input & Output

--- ukol-6.py
+++ ukol-6.py
@@ -13,10 +13,10 @@
 with open("text.txt", encoding="utf-8") as file:
     radky = file.readlines()
 
-uprava_znamek = []
+uprava_znamek = [radky[0]]
 
 for radek in radky[1:]:
-    uprava_znamek = radek.replace("1","A").replace("2","B").replace("3","C").replace("4","D").replace("5", "F")
+    uprava_znamek.append(radek.replace("1","A").replace("2","B").replace("3","C").replace("4","D").replace("5", "F")) 
     print(uprava_znamek)
 
 with open("text.txt", mode="w", encoding="utf-8") as output:
