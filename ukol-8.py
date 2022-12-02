e61c3f82c5bd4246fd6c2d0888736456911823b3
Pavlína Vencovská <pavlina.vencovska@gmail.com>
2022-12-02 14:38:23 +0100

    Pandas

--- ukol-8.py
+++ ukol-8.py
@@ -10,18 +10,22 @@
 # Které zvíře se nachází na záznamu s indexem 34? 
 # Vypiš pomocí iloc název tohoto zvířete v češtině i angličtině.
 
-# Bonus
-# V lekci jsme zmínili, že existují i jiné typy indexů, než jen číselný, který sám vyrobí pandas. Například v kontextu souboru se zvířaty se nabízí hned několik sloupců, které bychom mohli využít jako index, které nejsou číselné.
+# V lekci jsme zmínili, že existují i jiné typy indexů, než jen číselný, který sám vyrobí pandas. 
+# Například v kontextu souboru se zvířaty se nabízí hned několik sloupců, které bychom mohli využít jako index, které nejsou číselné.
 
-# Načti znovu data, ale tentokrát nastav parametr index_col na název sloupce, který obsahuje název zvířete v češtině. Všimni si, že sloupec teď povýší na index a už se nenachází mezi "běžnými" sloupci.
+# Načti znovu data, ale tentokrát nastav parametr index_col na název sloupce, který obsahuje název zvířete v češtině. 
+# Všimni si, že sloupec teď povýší na index a už se nenachází mezi "běžnými" sloupci.
 
 # Pomocí <tvoje-promenna>.index.is_unique ověř, zda je nový index unikátní.
 
-# Seřazený index je efektivnější a přehlednější. Seřaď index pomocí metody .sort_index(). Bacha, metoda vrátí novou tabulku se seřazeným indexem. Budeš tedy chtít přepsat původní dataframe.
+# Seřazený index je efektivnější a přehlednější. Seřaď index pomocí metody .sort_index(). 
+# Bacha, metoda vrátí novou tabulku se seřazeným indexem. Budeš tedy chtít přepsat původní dataframe.
 
-# Vyhledej řádek indexovaný názvem "Outloň váhavý". Namísto metody .iloc využij .loc.
+# Vyhledej řádek indexovaný názvem "Outloň váhavý". 
+# Namísto metody .iloc využij .loc.
 
-# Rozsahy fungují podobně jako u číselných indexů. Vyhledej záznamy mezi "Želva Smithova" a "Želva žlutočelá".
+# Rozsahy fungují podobně jako u číselných indexů. 
+# Vyhledej záznamy mezi "Želva Smithova" a "Želva žlutočelá".
 
 import pandas as pd
 
